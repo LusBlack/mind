0b5e5bf9719dd94179eae2059813233a90019bcf
Louis <Lou.ikwuegbu@gmail.com>
2023-02-18 11:44:25 +0100

    Da

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@
  use rand::Rng;
 fn main() {
     println!("Guess the number!");
-
-    let secret_number=rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: , {secret_number}");
-
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
     println!("Please input your guess.");
 
     let mut guess = String ::new();
